62eee9de8e6a64d048fd2fc263aa7e2c53df5ce8
mayurjawanjal <mjawanjal156@gmail.com>
2021-03-13 17:32:44 +0530

    example

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
+
+
+
 var app = angular.module("my-app",[]);
 
 app.controller("my-ctrl",($scope)=>{
-    $scope.title="Hello World!!!!";
+    $scope.title="Let's Travel!!";
+    var value=[];
+    
+    $scope.addCountry=()=>{
+    let data=localStorage.getItem("Country");
+    if(data){
+        value=JSON.parse(data);
+    }
+    value.push($scope.countryName);
+    localStorage.setItem("Country",JSON.stringify(value));
+    $scope.countryName="";
+    $scope.countryList=value;
+    };
+    
+    
 });
+
+
