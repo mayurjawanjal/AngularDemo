c17f474c21caa70706f2bad913bf0ae7a8e9a795
mayurjawanjal <mjawanjal156@gmail.com>
2021-03-13 18:05:19 +0530

    example fix

--- app.js
+++ app.js
@@ -6,19 +6,19 @@ var app = angular.module("my-app",[]);
 app.controller("my-ctrl",($scope)=>{
     $scope.title="Let's Travel!!";
     var value=[];
-    
-    $scope.addCountry=()=>{
     let data=localStorage.getItem("Country");
     if(data){
         value=JSON.parse(data);
     }
-    value.push($scope.countryName);
-    localStorage.setItem("Country",JSON.stringify(value));
-    $scope.countryName="";
     $scope.countryList=value;
+    $scope.addCountry=()=>{
+        if($scope.countryName){
+        value.push($scope.countryName);
+        localStorage.setItem("Country",JSON.stringify(value));
+        $scope.countryName="";
+        $scope.countryList=value;
+        }
     };
-    
-    
 });
 
 
