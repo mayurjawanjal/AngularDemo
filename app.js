fbcb31881716a4996cdb61479151107c4a410a74
mayurjawanjal <mjawanjal156@gmail.com>
2021-03-15 16:03:36 +0530

    fix

--- app.js
+++ app.js
@@ -4,21 +4,47 @@
 var app = angular.module("my-app",[]);
 
 app.controller("my-ctrl",($scope)=>{
-    $scope.title="Let's Travel!!";
-    var value=[];
-    let data=localStorage.getItem("Country");
+    
+    $scope.employeeList=[];
+    var id=0;
+    let data=localStorage.getItem("Capcom");
+
     if(data){
-        value=JSON.parse(data);
+        $scope.employeeList=JSON.parse(data);
+        id = parseInt($scope.employeeList[$scope.employeeList.length-1].__id);
     }
-    $scope.countryList=value;
-    $scope.addCountry=()=>{
-        if($scope.countryName){
-        value.push($scope.countryName);
-        localStorage.setItem("Country",JSON.stringify(value));
-        $scope.countryName="";
-        $scope.countryList=value;
-        }
+ 
+
+    $scope.addEmployee=()=>{
+        var temp;
+        temp=angular.copy($scope.employee);
+        temp.__id=id+1;
+
+        $scope.employeeList.push(temp);
+
+        localStorage.setItem("Capcom",angular.toJson($scope.employeeList));
+        $scope.employee.name="";
+        $scope.employee.dofb="";
+        $scope.employee.number="";
+        $scope.employee.mailID="";
+        $scope.employee.joiningDate="";
+        $scope.employee.department="";
+        $scope.employee.title="";
+        $scope.employee.address="";
+        $scope.employee.pincode=""; 
+        $scope.employee.salary="";
     };
+    
+    $scope.deleteEmployee=function(x){
+        for(let i=0;i<$scope.employeeList.length;i++){
+            if(x.__id===$scope.employeeList[i].__id)
+                $scope.employeeList.splice(i,1);
+        }
+        
+        if($scope.employeeList.length===0)
+            localStorage.removeItem("Capcom");
+        else
+            localStorage.getItem("Capcom",angular.toJson($scope.employeeList));
+    };
+
 });
-
-
