2c75d6e322558443f8e95f6ee82a151a4b5f3c21
mayurjawanjal <mjawanjal156@gmail.com>
2021-03-17 12:02:02 +0530

    http_service

--- app.js
+++ app.js
@@ -3,39 +3,39 @@
 
 var app = angular.module("my-app",[]);
 
-app.controller("my-ctrl",($scope)=>{
+app.controller("my-ctrl",($scope , $http)=>{
     
     $scope.employeeList=[];
-    var id=0;
-    let data=localStorage.getItem("Capcom");
+    //var id=0;
+    //let data=localStorage.getItem("Capcom");
 
+    /*
     if(data){
         $scope.employeeList=JSON.parse(data);
         id = parseInt($scope.employeeList[$scope.employeeList.length-1].__id);
     }
+    */
  
 
     $scope.addEmployee=()=>{
-        var temp;
-        temp=angular.copy($scope.employee);
-        temp.__id=id+1;
+        // var temp;
+        //temp=angular.copy($scope.employee);
 
-        $scope.employeeList.push(temp);
-
-        localStorage.setItem("Capcom",angular.toJson($scope.employeeList));
-        $scope.employee.name="";
-        $scope.employee.dofb="";
-        $scope.employee.number="";
-        $scope.employee.mailID="";
-        $scope.employee.joiningDate="";
-        $scope.employee.department="";
-        $scope.employee.title="";
-        $scope.employee.address="";
-        $scope.employee.pincode=""; 
-        $scope.employee.salary="";
+        //$scope.employeeList.push(temp);
+        $http.post( "http://localhost:4000/AddData" , angular.toJson($scope.employee))
+            .then(function(data){
+                $scope.employee={};
+                console.log(data);
+                //$scope.employeeList.push(data);
+                console.log('Employee added');
+            },function(data){
+                console.log('error '+ data);
+            });
+        //localStorage.setItem("Capcom",angular.toJson($scope.employeeList));
+        
     };
     
-    $scope.deleteEmployee=function(x){
+  /*  $scope.deleteEmployee=function(x){
         for(let i=0;i<$scope.employeeList.length;i++){
             if(x.__id===$scope.employeeList[i].__id)
                 $scope.employeeList.splice(i,1);
@@ -47,4 +47,7 @@ app.controller("my-ctrl",($scope)=>{
             localStorage.getItem("Capcom",angular.toJson($scope.employeeList));
     };
 
+    */
 });
+
+
